679f06c48970eb535e8a58980d732c6dcb68f3dd
unknown <r_faizaan@yahoo.ca>
2026-02-16 20:12:03 -0500

    Add metadata for setting tab icon

---
+++ src/app/metadata.ts
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: 'Restaurant Deal Scraper',
+  description: 'A web application that scrapes Instagram for restaurant deals',
+  icons: {
+    icon: '/icon.svg',
+  },
+};
